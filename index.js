26ebf53c70431e8094fea93bb4cbe7ad3f4d7c73
nguyenthanhcong <thanhcong150593@gmail.com>
2017-02-05 13:07:44 +0700

    firt commit on gitbub 9/2/2017

--- index.js
+++ index.js
@@ -9,6 +9,15 @@ app.set('views', './views');
 app.use(express.static('public'));
 
 
+
+
+app.get('/getdata', function(req, res){
+	var user = req.data;
+	 console.log("co truy cap toi route nay thong qua fetch API");
+
+	
+});
+
 app.get('/', function(req, res){
   res.render('index');
 })
--- src/components/List.jsx
+++ src/components/List.jsx
@@ -2,13 +2,28 @@ import React from 'react';
 import Note from './Note.jsx';
 import NoteForm from './NoteForm.jsx';
 
-var arr =[1,2,3];
+var arr=[1,2,3];
 
 class List extends React.Component{
   constructor(props){
     super(props);
     // this.state = {arr: ["a", "b", "c"] };
 
+  }
+  
+  componentDidMount() {
+		fetch('http://localhost:3000/getdata', {
+			method: 'GET',
+			data:  "nguyen thanh cong"
+			 })
+			.then(function(res){
+				return res.json()
+			}).then(function(body){
+				console.log(body)
+			})
+			.catch(function(err){
+				console.log("fetch error")
+			})
   }
   render(){
     var arrPlus =arr.map((i)=> <Note key={i}>Bài -{i}</Note>);
